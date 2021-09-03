0b653379bfe2d3c4c00d1f84dd4945d87107ad2a
Muhammad Humza <humza.jameel@evantagesoft.com>
2021-09-03 19:15:53 +0500

    login screen implemented

--- src/screens/Screens.js
+++ src/screens/Screens.js
@@ -2,16 +2,21 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Register from '../components/Register';
+import Login from '../components/Login';
+import Home from '../components/Home';
 
 const Stack = createNativeStackNavigator();
 
 const Screens = () => (
   <NavigationContainer>
     <Stack.Navigator
+      initialRouteName="Signin"
       screenOptions={{
         headerTitleAlign: 'center',
       }}>
-      <Stack.Screen name="Sign Up" component={Register} />
+      <Stack.Screen name="Signin" component={Login} />
+      <Stack.Screen name="Signup" component={Register} />
+      <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   </NavigationContainer>
 );
