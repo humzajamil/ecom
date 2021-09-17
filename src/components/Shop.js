75ac1b1a0a2c87f2f77af426d3e32b488fc82d86
Muhammad Humza <humza.jameel@evantagesoft.com>
2021-09-17 19:19:04 +0500

    timer for otp added

---
+++ src/components/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Text, Icon} from 'react-native-elements';
+
+const Shop = () => {
+  return (
+    <View>
+      <Text>Categories</Text>
+      <View></View>
+    </View>
+  );
+};
+
+export default Shop;
+
+const styles = StyleSheet.create({});
